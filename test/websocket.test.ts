ef8f60576eced727f9793757336c7b028e2cf2fc
Will Geraghty <will.geraghty@voicefoundry.com>
2022-03-04 17:14:28 -0700

    Initial commit

---
+++ test/websocket.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Websocket from '../lib/websocket-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/websocket-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Websocket.WebsocketStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
